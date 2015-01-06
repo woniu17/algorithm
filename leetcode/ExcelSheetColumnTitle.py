623af73c419189709481a87720b1bdd91b18abea
woniu17 <qinglucklin@foxmail.com>
2015-01-06 14:28:15 +0800

    submit problem Excel Sheet Column Title

---
+++ leetcode/ExcelSheetColumnTitle.py
@@ -0,0 +1,13 @@
+class Solution:
+	# @return a string
+	def convertToTitle(self, num):
+		title = ['A','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
+		qingluck = ''
+		while num>0:
+			tmp = num%26
+			if tmp == 0:
+				tmp = 26
+				num = num-26
+			num = num/26
+			qingluck = '%s%s'%(title[tmp], qingluck)
+		return qingluck
