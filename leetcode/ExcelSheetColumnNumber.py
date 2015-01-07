49dcbd7a55d7ea9f8134d7d5ad53c1abff4da0ee
woniu17 <qinglucklin@foxmail.com>
2015-01-07 18:43:20 +0800

    submit problem Excel Sheet Column Number

---
+++ leetcode/ExcelSheetColumnNumber.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param s, a string
+    # @return an integer
+    def titleToNumber(self, s):
+        title2number = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6,'G':7,'H':8,'I':9,'J':10,'K':11,'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17,'R':18,'S':19,'T':20,'U':21,'V':22,'W':23,'X':24,'Y':25,'Z':26,}
+        qingluck = 0
+        for i in xrange(len(s)):
+            qingluck = qingluck*26 + title2number[s[i]]
+        return qingluck
+if __name__ == '__main__':
+    s = 'AZ'
+    solution = Solution()
+    qingluck = solution.titleToNumber(s)
+    print 'result: %d'%qingluck
